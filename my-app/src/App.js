dffbd563e58914c8c22aa8611090042e680c183e
DESKTOP-HFSM6CN\Sebastian W 4TI <boogabooga310@gmail.com>
2024-09-27 10:46:39 +0200

    progres

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Tytuł filmu</label>
+          <input type="text" class="form-control" id="film" />
+        </div>
+      </form>
     </div>
   );
 }
